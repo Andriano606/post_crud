319b3468fecab8bedcb066047d28606021aa3aae
Andrii Kuluiev <andreykuluev@gmail.com>
2025-01-12 18:24:16 +0200

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 PostCrud::Engine.routes.draw do
-  PostCrud::Engine.routes.draw do
-    resources :posts, only: [ :new, :create, :index ]
-  end
+  resources :posts, only: [ :new, :create, :index ]
 end
